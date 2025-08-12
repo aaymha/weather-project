dbaceb5e38b843352fb5434ac621db1ab47a4396
simon304 <szymon7710@icloud.com>
2025-08-12 14:34:02 +0200

    api working, weather first step

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 st.set_page_config(page_title="MyWeather")
 api_key = "79d4c5cc409902a82858318bc7da2fd9"
-city = st.text_input("Enter city name: ")
+city = st.text_input("Enter city or country name: ")
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 response = requests.get(url)
