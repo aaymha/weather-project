a2a09bd1146f29801b46813c2cd78ee519a5b522
simon304 <szymon7710@icloud.com>
2025-08-12 14:18:05 +0200

    api working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import plotly as pt
 
 st.set_page_config(page_title="MyWeather")
 api_key = "79d4c5cc409902a82858318bc7da2fd9"
-city = st.text_input("Enter city name:")
+city = st.text_input("Enter city name: ")
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 response = requests.get(url)
