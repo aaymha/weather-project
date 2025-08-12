6fa15c6d6a7a91dd12b84e3279b4a9daf7e76036
simon304 <szymon7710@icloud.com>
2025-08-12 14:32:42 +0200

    api working, weather first step

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import plotly as pt
+import json
 
 st.set_page_config(page_title="MyWeather")
 api_key = "79d4c5cc409902a82858318bc7da2fd9"
@@ -9,4 +10,6 @@ city = st.text_input("Enter city name: ")
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 response = requests.get(url)
-st.write(response)
+weather_data = response.json()
+
+st.write(weather_data['name'] ,weather_data['main']['temp'])
